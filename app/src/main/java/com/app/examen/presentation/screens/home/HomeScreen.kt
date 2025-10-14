6d982f20225c08e55a5b5a522b5b2088a3704d44
Andrey <enriqueandrey@hotmail.com>
2025-10-14 13:59:46 -0600

    Boton

--- app/src/main/java/com/app/examen/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/app/examen/presentation/screens/home/HomeScreen.kt
@@ -3,8 +3,12 @@ package com.app.examen.presentation.screens.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -17,6 +21,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.app.examen.presentation.screens.home.components.BotonTexto
 import com.app.examen.presentation.screens.home.components.CountryListContent
 import com.app.examen.presentation.screens.home.components.SearchTab
 
@@ -27,21 +32,30 @@ fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
     var selectedTabIndex by remember { mutableStateOf(0) }
+    var showDialog by remember { mutableStateOf(false) }
     val tabs = listOf("Country List", "Search")
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    if (showDialog) {
+        BotonTexto(onDismiss = { showDialog = false })
+    }
 
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
                 title = { Text("Countries") },
+                actions = {
+                    IconButton(onClick = { showDialog = true }) {
+                        Icon(Icons.Default.Info, "Info")
+                    }
+                }
             )
         },
     ) { padding ->
         Column(
-            modifier =
-                Modifier
-                    .fillMaxSize()
-                    .padding(padding),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
         ) {
             TabRow(selectedTabIndex = selectedTabIndex) {
                 tabs.forEachIndexed { index, title ->
---
+++ app/src/main/java/com/app/examen/presentation/screens/home/components/BotonTexto.kt
@@ -0,0 +1,43 @@
+package com.app.examen.presentation.screens.home.components
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun BotonTexto(onDismiss: () -> Unit) {
+    Dialog(onDismissRequest = onDismiss) {
+        Surface(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    "Arquitectura",
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Text(
+                    """
+                    MVVM + Clean Architecture
+                    - CountryRepository define cómo obtener países
+                    
+                    Estrategia de guardado de preferencias
+                    - Guardamos datos locales del usuario
+                    
+                    Búsqueda
+                    - Filtramos países en memoria por nombre, oficial y capital
+                    """.trimIndent(),
+                    modifier = Modifier.padding(16.dp)
+                )
+                Button(onClick = onDismiss, modifier = Modifier.padding(8.dp)) {
+                    Text("Ok")
+                }
+            }
+        }
+    }
+}
